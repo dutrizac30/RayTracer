28eab48158f19260376f720a4bcc656f3f145ec3
Jay Steele <jaydsteele@gmail.com>
2022-06-23 22:32:51 -0300

    Add some comments on how to implement a few more of the functions

--- vec3.h
+++ vec3.h
@@ -3,9 +3,9 @@
 
 struct vec3
 {
-    double x;
-    double y;
-    double z;
+  double x;
+  double y;
+  double z;
 };
 
 void vec3_set(double x, double y, double z, struct vec3 *result);
@@ -24,11 +24,12 @@ void vec3_divscalar(struct vec3 *v, double t, struct vec3 *result);
 
 /**
 // Here are the functions we need to implement
-// double vec3_dot(v, w)
+// double vec3_dot(v, w) == v.x * w.x + v.y * w.y + v.z * w.z
+// double vec3_len_squared(v) == vec3_dot(v, v)
+// double vec3_len(v) == sqrt(vec3_len_squared(v))
+// void vec3_unit(v, r) { vec3_divscalar(v, vec3_len(v), r) }
+//
 // void vec3_cross(v, w, r)
-// double vec3_len_squared(v)
-// double vec3_len(v)
-// void vec3_unit(v, w)
 */
 
 #endif
